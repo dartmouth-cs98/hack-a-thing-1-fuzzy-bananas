33ec62e0a87c1b72161fa8d2028e38075f3abf88
Stephanie <stephanie@Stephanies-MacBook-Air.local>
2017-09-18 22:21:03 -0400

    Added random card generation from clicking deck holy crap why was this so difficult

--- Assets/_Completed-Assets/Scripts/Card.cs
+++ Assets/_Completed-Assets/Scripts/Card.cs
@@ -7,34 +7,47 @@ public class Card : MonoBehaviour {
 	private int _rank;
 	public int Rank { get { return _rank; } }
 
-//	private GameObject _card;
+	private GameObject _card;
 
 	void Start () 
 	{
 		transform.position = new Vector3(0, 0, 0);
 	}
 
-	public Card(int rank) {
-		
-		string assetName = string.Format("card_{0}", rank);  // Example:  "card_2" is card with number 2: 2 to 99
-		GameObject asset = GameObject.Find(assetName);
-		if (asset == null) {
-			Debug.LogError("Asset '" + assetName + "' could not be found.");
-		} else {
-			_rank = rank;
-			Debug.Log ("saysomething");
-			makeCardVisible (asset);
-		}
-	}
+//	public Card(int rank) {
+//		Debug.Log ("making new card of rank " + rank);
+//		string assetName = string.Format("card_{0}", rank);  // Example:  "card_2" is card with number 2: 2 to 99
+//		GameObject asset = GameObject.Find(assetName);
+//		if (asset == null) {
+//			Debug.LogError("Asset '" + assetName + "' could not be found.");
+//		} else {
+//			_rank = rank;
+//			Debug.Log ("card with rank" + rank + " was found");
+//			makeCardVisible (asset);
+//		}
+//	}
 
-	public void makeCardVisible(GameObject GO)
+	public void makeCardVisible(int _rank)
 	{
 //		GO.layer = LayerMask.NameToLayer ("player");
-		GO.layer = 31;
+		string assetName = string.Format("card_{0}", _rank);  // Example:  "card_2" is card with number 2: 2 to 99
+		_card = GameObject.Find(assetName);
+		if (_card == null) {
+			Debug.LogError("Asset '" + assetName + "' could not be found.");
+		} else {
+//			_rank = _rank;
+			Debug.Log("making card visible :)");
+			_card.layer = 31;
+
+			Vector3 newPosition = new Vector3(100, 100, 10.0f);
+			transform.position = Camera.main.ScreenToWorldPoint(newPosition);
+
+		}
 	}
 
 	void OnMouseDrag()
 	{
+		Debug.Log ("mouse position is " + Input.mousePosition.x + " and " + Input.mousePosition.y);
 		Vector3 newPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10.0f);
 		transform.position = Camera.main.ScreenToWorldPoint(newPosition);
 	}
--- Assets/_Completed-Assets/Scripts/Deck.cs
+++ Assets/_Completed-Assets/Scripts/Deck.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Deck : MonoBehaviour {
 	private List<int> _deck = new List<int>();
 	private List<Card> _onTable = new List<Card>();
+	public GameObject deck;
 
 	// Use this for initialization
 	void Start () 
@@ -18,13 +19,33 @@ public class Deck : MonoBehaviour {
 
 	void OnMouseDown() 
 	{
-		Debug.Log ("saowiaejfawijg");
 		int i = Random.Range (0, _deck.Count - 1);
 		int cardRank = _deck [i];
 		_deck.RemoveAt (i);
 
-		Card card = new Card (cardRank);
+		Debug.Log ("OnMouseDown of Deck.cs called, rank is " + cardRank);
+
+//		Card card = new Card (cardRank);
+//		Card card = deck.AddComponent<Card>();
+		Card card = getCard(cardRank);
 		_onTable.Add (card);
+	}
+
+	Card getCard(int _rank) {
+
+		string assetName = string.Format("card_{0}", _rank);  // Example:  "card_2" is card with number 2: 2 to 99
+		GameObject cardObj = GameObject.Find(assetName);
+		if (cardObj == null) {
+			Debug.LogError("Asset '" + assetName + "' could not be found.");
+			return null;
+		} else {
+//			_rank = _rank;
+			Debug.Log ("card with rank" + _rank + " was found");
+			Card card = cardObj.GetComponent<Card>();
+			Debug.Log (card);
+			card.makeCardVisible(_rank);
+			return card;
+		}
 	}
 
 }
